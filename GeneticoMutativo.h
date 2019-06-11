69aca97dbd9b54bf756888e9923fe6dce47b2c48
Jose Alberto Solano <joseasolano99@gmail.com>
2019-06-11 00:07:04 -0600

    Mutativo.h

---
+++ GeneticoMutativo.h
@@ -0,0 +1,13 @@
+#ifndef ALGORITMOMUTATIVO
+#define ALGORITMOMUTATIVO
+#include "Poblacion.h"
+#include "Algoritmo.h"
+#include<ctime>
+#define PROBABILIDAD_MUTUACION 50
+class AlgoritmoMutativo: public Algoritmo {
+	public:
+	   AlgoritmoMutativo();
+       Poblacion * evolucionar( Poblacion * );
+};
+
+#endif
